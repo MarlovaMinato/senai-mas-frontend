b580f0fdcb61c110c711eca09bdf864d2a3f1422
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-11-06 19:02:29 -0300

    Adjustment

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Teste} from './pages/Teste'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Frontend My Activies Space</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Frontend My Activies Space</h1>
+            <Teste />
+        </div>
+    );
 }
 
 export default App;
