36bf2928bd4de25943cc7222f3e4c13b863442bf
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-11-06 22:10:23 -0300

    Update
    Login; NotFound

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+import { NotFound } from '../pag/NotFound';
 
 const Routes: React.FC = () => (
     <Switch>
-        <Route />
+        <Route path="/" exact component={Login} />
+        <Route path="/*" component={NotFound} />
     </Switch>
 )
 
