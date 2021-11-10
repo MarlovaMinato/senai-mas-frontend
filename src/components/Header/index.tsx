3a58f7b2f2a0e88889188087ea86a55a9e437a05
MarlovaMinato <82427141+MarlovaMinato@users.noreply.github.com>
2021-11-10 13:38:57 -0300

    Updade
    Comunicação entre componentes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,26 @@
 import { Container, Content} from './styles';
 
+nterface HeaderProps {
+    onOpenNewActivyModal: () => void;
+    onOpenNewCourseUnitModal: () => void;
+}
 
-export function Header(){
+export function Header({onOpenNewActivyModal, onOpenNewCourseUnitModal}:HeaderProps){
     return(
         <Container>
             <Content>
                 <h1>My Activies Space</h1>
                 <div>
-                    <button> type="button">
+                    <button> 
+                        type="button"
+                        onClick={onOpenNewCourseUnitModal}
+                    >
                         Nova Unidade Curricular
                     </button>
-                    <button type="button"> 
+                    <button 
+                        type="button"
+                        onClick={onOpenNewActivyModal}
+                    > 
                         Nova Atividade               
                     </button>
                 </div>
